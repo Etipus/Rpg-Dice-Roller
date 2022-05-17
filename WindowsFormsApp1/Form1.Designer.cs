702eb1a3aab3a6e0b8d72565411142584f16d783
Enes <etipuss@gmail.com>
2022-05-17 14:07:25 +0300

    ...

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class RDR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RDR));
+            this.d8label = new System.Windows.Forms.Label();
+            this.d10label = new System.Windows.Forms.Label();
+            this.d12label = new System.Windows.Forms.Label();
+            this.d20label = new System.Windows.Forms.Label();
+            this.d100label = new System.Windows.Forms.Label();
+            this.d8button = new System.Windows.Forms.Button();
+            this.d10button = new System.Windows.Forms.Button();
+            this.d12button = new System.Windows.Forms.Button();
+            this.d20button = new System.Windows.Forms.Button();
+            this.d100button = new System.Windows.Forms.Button();
+            this.exıtbutton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // d8label
+            // 
+            this.d8label.BackColor = System.Drawing.SystemColors.Control;
+            this.d8label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.d8label.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.d8label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.d8label.Location = new System.Drawing.Point(30, 173);
+            this.d8label.Name = "d8label";
+            this.d8label.Size = new System.Drawing.Size(37, 17);
+            this.d8label.TabIndex = 0;
+            // 
+            // d10label
+            // 
+            this.d10label.BackColor = System.Drawing.SystemColors.Control;
+            this.d10label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.d10label.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.d10label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.d10label.Location = new System.Drawing.Point(197, 173);
+            this.d10label.Name = "d10label";
+            this.d10label.Size = new System.Drawing.Size(38, 17);
+            this.d10label.TabIndex = 1;
+            // 
+            // d12label
+            // 
+            this.d12label.BackColor = System.Drawing.SystemColors.Control;
+            this.d12label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.d12label.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.d12label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.d12label.Location = new System.Drawing.Point(379, 173);
+            this.d12label.Name = "d12label";
+            this.d12label.Size = new System.Drawing.Size(38, 17);
+            this.d12label.TabIndex = 2;
+            // 
+            // d20label
+            // 
+            this.d20label.BackColor = System.Drawing.SystemColors.Control;
+            this.d20label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.d20label.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.d20label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.d20label.Location = new System.Drawing.Point(552, 173);
+            this.d20label.Name = "d20label";
+            this.d20label.Size = new System.Drawing.Size(38, 17);
+            this.d20label.TabIndex = 3;
+            // 
+            // d100label
+            // 
+            this.d100label.BackColor = System.Drawing.SystemColors.Control;
+            this.d100label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.d100label.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.d100label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.d100label.Location = new System.Drawing.Point(732, 174);
+            this.d100label.Name = "d100label";
+            this.d100label.Size = new System.Drawing.Size(38, 17);
+            this.d100label.TabIndex = 4;
+            // 
+            // d8button
+            // 
+            this.d8button.Location = new System.Drawing.Point(12, 52);
+            this.d8button.Name = "d8button";
+            this.d8button.Size = new System.Drawing.Size(75, 81);
+            this.d8button.TabIndex = 5;
+            this.d8button.Text = "button1";
+            this.d8button.UseVisualStyleBackColor = true;
+            this.d8button.Click += new System.EventHandler(this.d8button_Click);
+            // 
+            // d10button
+            // 
+            this.d10button.Location = new System.Drawing.Point(177, 52);
+            this.d10button.Name = "d10button";
+            this.d10button.Size = new System.Drawing.Size(75, 81);
+            this.d10button.TabIndex = 6;
+            this.d10button.Text = "button2";
+            this.d10button.UseVisualStyleBackColor = true;
+            this.d10button.Click += new System.EventHandler(this.d10button_Click);
+            // 
+            // d12button
+            // 
+            this.d12button.Location = new System.Drawing.Point(359, 52);
+            this.d12button.Name = "d12button";
+            this.d12button.Size = new System.Drawing.Size(75, 81);
+            this.d12button.TabIndex = 7;
+            this.d12button.Text = "button3";
+            this.d12button.UseVisualStyleBackColor = true;
+            this.d12button.Click += new System.EventHandler(this.d12button_Click);
+            // 
+            // d20button
+            // 
+            this.d20button.Location = new System.Drawing.Point(534, 52);
+            this.d20button.Name = "d20button";
+            this.d20button.Size = new System.Drawing.Size(75, 81);
+            this.d20button.TabIndex = 8;
+            this.d20button.Text = "button4";
+            this.d20button.UseVisualStyleBackColor = true;
+            this.d20button.Click += new System.EventHandler(this.d20button_Click);
+            // 
+            // d100button
+            // 
+            this.d100button.Location = new System.Drawing.Point(713, 52);
+            this.d100button.Name = "d100button";
+            this.d100button.Size = new System.Drawing.Size(75, 81);
+            this.d100button.TabIndex = 9;
+            this.d100button.Text = "button5";
+            this.d100button.UseVisualStyleBackColor = true;
+            this.d100button.Click += new System.EventHandler(this.d100button_Click);
+            // 
+            // exıtbutton
+            // 
+            this.exıtbutton.Location = new System.Drawing.Point(359, 365);
+            this.exıtbutton.Name = "exıtbutton";
+            this.exıtbutton.Size = new System.Drawing.Size(75, 23);
+            this.exıtbutton.TabIndex = 10;
+            this.exıtbutton.Text = "Exıt";
+            this.exıtbutton.UseVisualStyleBackColor = true;
+            this.exıtbutton.Click += new System.EventHandler(this.exıtbutton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(177, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(76, 81);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox3.Enabled = false;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(359, 52);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox4.Enabled = false;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(534, 52);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox5.Enabled = false;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(713, 52);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 81);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RDR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.exıtbutton);
+            this.Controls.Add(this.d100button);
+            this.Controls.Add(this.d20button);
+            this.Controls.Add(this.d12button);
+            this.Controls.Add(this.d10button);
+            this.Controls.Add(this.d8button);
+            this.Controls.Add(this.d100label);
+            this.Controls.Add(this.d20label);
+            this.Controls.Add(this.d12label);
+            this.Controls.Add(this.d10label);
+            this.Controls.Add(this.d8label);
+            this.Name = "RDR";
+            this.Text = "Rpg Dice Roller";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label d8label;
+        private System.Windows.Forms.Label d10label;
+        private System.Windows.Forms.Label d12label;
+        private System.Windows.Forms.Label d20label;
+        private System.Windows.Forms.Label d100label;
+        private System.Windows.Forms.Button d8button;
+        private System.Windows.Forms.Button d10button;
+        private System.Windows.Forms.Button d12button;
+        private System.Windows.Forms.Button d20button;
+        private System.Windows.Forms.Button d100button;
+        private System.Windows.Forms.Button exıtbutton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class RDR : Form
+    {
+        public RDR()
+        {
+            InitializeComponent();
+        }
+
+        private void d8button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 9);
+
+            d8label.Text = randomnumber.ToString();
+        }
+
+        private void d10button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 11);
+
+            d10label.Text = randomnumber.ToString();
+        }
+
+        private void d12button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 13);
+
+            d12label.Text = randomnumber.ToString();
+        }
+
+        private void d20button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 21);
+
+            d20label.Text = randomnumber.ToString();
+        }
+
+        private void d100button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 101);
+
+            d100label.Text = randomnumber.ToString();
+        }
+
+        private void exıtbutton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
